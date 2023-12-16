b3dbaac0cb06f8ea3a058aa7b917ae598b200d0d
Yevhenii Sotnyk <sotnikzhe@gmail.com>
2023-12-16 16:19:37 +0200

    Use header for functions

---
+++ functions.cpp
@@ -0,0 +1,25 @@
+#include "functions.h"
+
+// Substract two values
+int SubstractFunc(int num1, int num2)
+{
+	return num1 - num2;
+}
+
+// Sum two values
+int SumFunc(int num1, int num2)
+{
+	return num1 + num2;
+}
+
+// Multiply two values
+int MultFunc(int num1, int num2)
+{
+	return num1 * num2;
+}
+
+// Divide two values
+int DivideFunc(int num1, int num2)
+{
+	return num1 / num2;
+}
