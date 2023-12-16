275137a89cfe99a02c7851ee5746742abd79fbdb
Yevhenii Sotnyk <sotnikzhe@gmail.com>
2023-12-16 16:19:37 +0200

    Use header for functions

---
+++ functions.h
@@ -0,0 +1,11 @@
+// Substract two values
+int SubstractFunc(int num1, int num2);
+
+// Sum two values
+int SumFunc(int num1, int num2);
+
+// Multiply two values
+int MultFunc(int num1, int num2);
+
+// Divide two values
+int DivideFunc(int num1, int num2);
