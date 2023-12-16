f05bd6ffab9969c754fd97e0c92f32e06c5cc367
Yevhenii Sotnyk <sotnikzhe@gmail.com>
2023-12-16 16:15:29 +0200

    Add description for functions

--- main.cpp
+++ main.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 
+// Substract two values
 int SubstractFunc(int num1, int num2)
 {
 	return num1 - num2;
 }
+
+// Sum two values
 int SumFunc(int num1, int num2)
 {
 	return num1 + num2;
 }
+
+// Multiply two values
 int MultFunc(int num1, int num2)
 {
 	return num1 * num2;
 }
+
+// Divide two values
 int DivideFunc(int num1, int num2)
 {
 	return num1 / num2;
