bbd7cb848f29cfffc808211f364c66f2a0e1dcbb
Yevhenii Sotnyk <sotnikzhe@gmail.com>
2023-12-16 16:18:47 +0200

    Remove functions from main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,5 @@
 #include <iostream>
-
-// Substract two values
-int SubstractFunc(int num1, int num2)
-{
-	return num1 - num2;
-}
-
-// Sum two values
-int SumFunc(int num1, int num2)
-{
-	return num1 + num2;
-}
-
-// Multiply two values
-int MultFunc(int num1, int num2)
-{
-	return num1 * num2;
-}
-
-// Divide two values
-int DivideFunc(int num1, int num2)
-{
-	return num1 / num2;
-}
+#include "functions.h"
 
 int main(void)
 {
