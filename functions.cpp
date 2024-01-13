0c37a1b8b01090d3e5818c38a3071e2b47c448a3
Yevhenii Sotnyk <sotnikzhe@gmail.com>
2024-01-13 18:42:06 +0200

    Few changes

--- functions.cpp
+++ functions.cpp
@@ -3,23 +3,23 @@
 // Substract two values
 int SubstractFunc(int num1, int num2)
 {
-	return num1 - num2;
+    return num1 - num2;
 }
 
 // Sum two values
 int SumFunc(int num1, int num2)
 {
-	return num1 + num2;
+    return num1 + num2;
 }
 
 // Multiply two values
 int MultFunc(int num1, int num2)
 {
-	return num1 * num2;
+    return num1 * num2;
 }
 
 // Divide two values
 int DivideFunc(int num1, int num2)
 {
-	return num1 / num2;
+    return num1 / num2;
 }
